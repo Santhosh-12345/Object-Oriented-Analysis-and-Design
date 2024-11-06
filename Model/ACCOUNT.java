45010300e9a1f9daad3eeffa7dcdfdfeda7b11cc
Santhosh S <115558976+Santhosh-12345@users.noreply.github.com>
2024-11-06 19:31:22 +0530

    Add files via upload

---
+++ Model/ACCOUNT.java
@@ -0,0 +1,38 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class ACCOUNT {
+
+    /**
+     * Default constructor
+     */
+    public ACCOUNT() {
+    }
+
+    /**
+     * 
+     */
+    public void No_Borrowed_Books;
+
+    /**
+     * 
+     */
+    public void No_Lost_Books;
+
+    /**
+     * 
+     */
+    public void No_Returned_Books;
+
+    /**
+     * 
+     */
+    public void Calculate_Fine() {
+        // TODO implement here
+    }
+
+}
---
+++ Model/LIBRARIAN.java
@@ -0,0 +1,52 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class LIBRARIAN {
+
+    /**
+     * Default constructor
+     */
+    public LIBRARIAN() {
+    }
+
+    /**
+     * 
+     */
+    public void Name;
+
+    /**
+     * 
+     */
+    public void ID;
+
+    /**
+     * 
+     */
+    public void PhNo;
+
+    /**
+     * 
+     */
+    public void Check_In_Book() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Check_Out_Book() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Manage_Catalog() {
+        // TODO implement here
+    }
+
+}
