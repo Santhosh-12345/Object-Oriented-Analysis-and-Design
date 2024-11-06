a84cc5e5fede1981ce8cb21d6ebe4cbef796e1b2
Santhosh S <115558976+Santhosh-12345@users.noreply.github.com>
2024-11-06 19:41:02 +0530

    Add files via upload

---
+++ Mapping_Design_To-Code/LIBRARY.java
@@ -0,0 +1,40 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class LIBRARY {
+
+    /**
+     * Default constructor
+     */
+    public LIBRARY() {
+    }
+
+    /**
+     * 
+     */
+    public void Name;
+
+    /**
+     * 
+     */
+    public void Address;
+
+    /**
+     * 
+     */
+    public void Add_Book() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Remove_Book() {
+        // TODO implement here
+    }
+
+}
---
+++ Mapping_Design_To-Code/USER.java
@@ -0,0 +1,59 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class USER {
+
+    /**
+     * Default constructor
+     */
+    public USER() {
+    }
+
+    /**
+     * 
+     */
+    public void Name;
+
+    /**
+     * 
+     */
+    public void ID;
+
+    /**
+     * 
+     */
+    public void PhoneNo;
+
+    /**
+     * 
+     */
+    public void Borrow_Book() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Return_Book() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Pay_Fine() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Renew_Book() {
+        // TODO implement here
+    }
+
+}
